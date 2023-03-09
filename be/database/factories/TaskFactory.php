5107a087321dcbe5144a226af6b202655c78cda6
--replace-all <miqayelpetrosyan@gmail.com>
2023-03-09 14:17:33 +0400

    fullname length, factory seeder

---
+++ be/database/factories/TaskFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'created_by' => User::inRandomOrder()->first()->id,
+            'assigned_to' => User::inRandomOrder()->first()->id,
+            'title' => $this->faker->text(10),
+            'description' => $this->faker->text(150),
+            'status' => array_rand([0, 1, 2, 3], 1),
+            'created_at' => now(),
+        ];
+    }
+}
---
+++ be/database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::firstOrCreate(['email' => 'john@gmail.com'], [
+            'full_name' => 'John Doe',
+            'email' => 'john@gmail.com',
+            'password' => '12345678',
+        ]);
+
+        User::factory(10)->create();
+    }
+}
